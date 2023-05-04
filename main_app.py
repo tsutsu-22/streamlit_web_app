a82c2c5d94ab322fbe8f3c600f0587e2c4054464
tsutsu-22 <106950932+tsutsu-22@users.noreply.github.com>
2023-05-04 20:17:55 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title('サンプルアプリ')
-st.caption('これはサンプルアプリです')
+st.caption('これはサンプルアプリです.')
 
 img = Image.open('./data/cat.jpg')
 st.image(img,width=200)
